b11cfdf33570944fc8c4af9cd22e2374c2e9bbce
hyoil Lee <dlgydlf123@gmail.com>
2024-06-16 18:20:47 +0900

    update: js -> ts complie

---
+++ index.js
@@ -0,0 +1,10 @@
+var hello = "hello";
+console.log(hello);
+var add = function (a, b) {
+    return a + b;
+};
+var Car = /** @class */ (function () {
+    function Car() {
+    }
+    return Car;
+}());
---
+++ index.ts
@@ -0,0 +1,8 @@
+let hello: string = "hello";
+console.log(hello);
+
+const add = (a: number, b: number): number => {
+  return a + b
+}
+
+class Car {}
